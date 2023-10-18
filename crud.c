c0a6532f71ba14875ddea056adf36443d4cee362
Leonardo Machado Baptista <lmbaudin2013@gmail.com>
2023-10-18 20:28:32 -0300

    Sistema CRUD no contexto do RH

--- crud.c
+++ crud.c
@@ -3,22 +3,22 @@
 #include <string.h>
 #include <unistd.h>
 
-// Definindo uma estrutura para representar um livro
-typedef struct livro
+// Definindo a estrutura do funcionario
+typedef struct funcionario
 {
-    char titulo[100];
-    char autor[100];
+    char cpf[11];
+    char nome[100];
+    char cargo[100];
     int mes;
     int ano;
-    char idioma[20];
-    char tipo[20];
-    struct livro *prox;
-} Livro;
+    float salario;
+    struct funcionario *prox;
+} Funcionario;
 
-// Definindo uma estrutura para representar uma lista encadeada de livros
+// Definindo uma estrutura para representar uma lista encadeada
 typedef struct lista
 {
-    Livro *inicio;
+    Funcionario *inicio;
     int tamanho;
 } Lista;
 
@@ -31,64 +31,64 @@ Lista *criar_lista()
     return lista;
 }
 
-// Criando uma função para liberar a memória de uma lista
+// Criando uma função para liberar a memoria de uma lista
 void liberar_lista(Lista *lista)
 {
-    Livro *aux = lista->inicio;
+    Funcionario *aux = lista->inicio;
     while (aux != NULL)
     {
-        Livro *prox = aux->prox;
+        Funcionario *prox = aux->prox;
         free(aux);
         aux = prox;
     }
     free(lista);
 }
 
-// Criando uma função para inserir um livro no início da lista
-void inserir_inicio(Lista *lista, Livro *livro)
+// Criando uma função para inserir um funcionario no inicio da lista
+void inserir_inicio(Lista *lista, Funcionario *funcionario)
 {
-    livro->prox = lista->inicio;
-    lista->inicio = livro;
+    funcionario->prox = lista->inicio;
+    lista->inicio = funcionario;
     lista->tamanho++;
 }
 
-// Criando uma função para inserir um livro no final da lista
-void inserir_final(Lista *lista, Livro *livro)
+// Criando uma função para inserir um funcionario no final da lista
+void inserir_final(Lista *lista, Funcionario *funcionario)
 {
     if (lista->inicio == NULL)
     {
-        inserir_inicio(lista, livro);
+        inserir_inicio(lista, funcionario);
     }
     else
     {
-        Livro *aux = lista->inicio;
+        Funcionario *aux = lista->inicio;
         while (aux->prox != NULL)
         {
             aux = aux->prox;
         }
-        aux->prox = livro;
-        livro->prox = NULL;
+        aux->prox = funcionario;
+        funcionario->prox = NULL;
         lista->tamanho++;
     }
 }
 
-// Criando uma função para remover um livro da lista pelo título
-int remover_titulo(Lista *lista, char *titulo)
+// Criando uma função para remover um funcionario da lista pelo cpf
+int remover_funcionario(Lista *lista, char *cpf)
 {
     if (lista->inicio == NULL)
     {
         return 0; // Lista vazia
     }
-    Livro *ant = NULL;
-    Livro *aux = lista->inicio;
-    while (aux != NULL && strcmp(aux->titulo, titulo) != 0)
+    Funcionario *ant = NULL;
+    Funcionario *aux = lista->inicio;
+    while (aux != NULL && strcmp(aux->cpf, cpf) != 0)
     {
         ant = aux;
         aux = aux->prox;
     }
     if (aux == NULL)
     {
-        return 0; // Livro não encontrado
+        return 0; // cpf não encontrado
     }
     if (ant == NULL)
     {
@@ -100,69 +100,69 @@ int remover_titulo(Lista *lista, char *titulo)
     }
     free(aux);
     lista->tamanho--;
-    return 1; // Livro removido com sucesso
+    return 1; // cpf removido com sucesso
 }
 
-// Criando uma função para buscar um livro na lista pelo título
-Livro *buscar_titulo(Lista *lista, char *titulo)
+// Criando uma função para buscar um funcionario na lista pelo cpf
+Funcionario *buscar_funcionario(Lista *lista, char *cpf)
 {
-    Livro *aux = lista->inicio;
-    while (aux != NULL && strcmp(aux->titulo, titulo) != 0)
+    Funcionario *aux = lista->inicio;
+    while (aux != NULL && strcmp(aux->cpf, cpf) != 0)
     {
         aux = aux->prox;
     }
-    return aux; // Retorna o livro encontrado ou NULL se não encontrar
+    return aux; // Retorna o funcionario encontrado ou NULL se nÃ£o encontrar
 }
 
-// Criando uma função para imprimir um livro na tela
-void imprimir_livro(Livro *livro)
+// Criando uma função para imprimir o funcionario na tela
+void imprimir_funcionario(Funcionario *funcionario)
 {
-    printf("Título: %s\n", livro->titulo);
-    printf("Autor: %s\n", livro->autor);
-    printf("Mês: %d\n", livro->mes);
-    printf("Ano: %d\n", livro->ano);
-    printf("Idioma: %s\n", livro->idioma);
-    printf("Tipo: %s\n", livro->tipo);
+    printf("Cpf: %s\n", funcionario->cpf);
+    printf("Nome: %s\n", funcionario->nome);
+    printf("Cargo: %s\n", funcionario->cargo);
+    printf("Mes: %d\n", funcionario->mes);
+    printf("Ano: %d\n", funcionario->ano);
+    printf("Salario: %f\n", funcionario->salario);
 }
 
-// Criando uma função para imprimir a lista de livros na tela
+// Criando uma função para imprimir a lista de funcionarios na tela
 void imprimir_lista(Lista *lista)
 {
-    Livro *aux = lista->inicio;
+    Funcionario *aux = lista->inicio;
     while (aux != NULL)
     {
-        imprimir_livro(aux);
+        imprimir_funcionario(aux);
         printf("\n");
         aux = aux->prox;
     }
 }
 
-// Criando uma função para ler os dados de um livro a partir do teclado
-Livro *ler_livro()
+// Criando uma função para ler os dados de um funcionario a partir do teclado
+Funcionario *ler_funcionario()
 {
-    Livro *livro = (Livro *)malloc(sizeof(Livro));
-    printf("Digite o título do livro: ");
-    scanf("%[^\n]", livro->titulo);
+    Funcionario *funcionario = (Funcionario *)malloc(sizeof(Funcionario));
+    printf("Digite o cpf do funcionario: ");
+    scanf("%[^\n]", funcionario->cpf);
     getchar();
-    printf("Digite o autor do livro: ");
-    scanf("%[^\n]", livro->autor);
+    printf("Digite o nome do funcionario: ");
+    scanf("%[^\n]", funcionario->nome);
     getchar();
-    printf("Digite o mês de publicação do livro: ");
-    scanf(" %d", &livro->mes);
+    printf("Digite o mes de admissao do funcionario: ");
+    scanf(" %d", &funcionario->mes);
     getchar();
-    printf("Digite o ano de publicação do livro: ");
-    scanf(" %d", &livro->ano);
+    printf("Digite o ano de admissao do funcionario: ");
+    scanf(" %d", &funcionario->ano);
     getchar();
-    printf("Digite o idioma do livro: ");
-    scanf("%[^\n]", livro->idioma);
+    printf("Digite o salario do funcionario: ");
+    scanf(" %f", &funcionario->salario);
     getchar();
-    printf("Digite o tipo do livro: ");
-    scanf("%[^\n]", livro->tipo);
+    printf("Digite o cargo do funcionario: ");
+    scanf("%[^\n]", funcionario->cargo);
     getchar();
-    return livro;
+    return funcionario;
 }
 
-// Criando uma função para carregar os dados dos livros a partir de um arquivo
+// Criando uma função para carregar os dados dos funcionarios a partir de um arquivo
 Lista *carregar_arquivo(char *nome_arquivo)
 {
     Lista *lista = criar_lista();
@@ -173,29 +173,29 @@ Lista *carregar_arquivo(char *nome_arquivo)
         return lista;
     }
     char linha[300];
-    fgets(linha, 300, arq); // Ignorando a primeira linha do arquivo (cabeçalho)
+    fgets(linha, 300, arq); // Ignorando a primeira linha do arquivo (cabecalho)
     while (fgets(linha, 300, arq) != NULL)
     {
-        Livro *livro = (Livro *)malloc(sizeof(Livro));
+        Funcionario *funcionario = (Funcionario *)malloc(sizeof(Funcionario));
         char *token = strtok(linha, ";");
-        strcpy(livro->titulo, token);
+        strcpy(funcionario->cpf, token);
         token = strtok(NULL, ";");
-        strcpy(livro->autor, token);
+        strcpy(funcionario->nome, token);
         token = strtok(NULL, ";");
-        livro->mes = atoi(token);
+        funcionario->mes = atoi(token);
         token = strtok(NULL, ";");
-        livro->ano = atoi(token);
+        funcionario->ano = atoi(token);
         token = strtok(NULL, ";");
-        strcpy(livro->idioma, token);
+        funcionario->salario = atof(token);
         token = strtok(NULL, "\n");
-        strcpy(livro->tipo, token);
-        inserir_final(lista, livro);
+        strcpy(funcionario->cargo, token);
+        inserir_final(lista, funcionario);
     }
     fclose(arq);
     return lista;
 }
 
-// Criando uma função para salvar os dados dos livros em um arquivo
+// Criando uma função para salvar os dados dos funcionarios em um arquivo
 void salvar_arquivo(char *nome_arquivo, Lista *lista)
 {
     FILE *arq = fopen(nome_arquivo, "w");
@@ -204,11 +204,11 @@ void salvar_arquivo(char *nome_arquivo, Lista *lista)
         printf("Erro ao abrir o arquivo.\n");
         return;
     }
-    fprintf(arq, "titulo_livro;nome_autor;mes;ano;idioma;tipo_livro\n"); // Escrevendo o cabeçalho do arquivo
-    Livro *aux = lista->inicio;
+    fprintf(arq, "cpf;nome_func;mes;ano;salario;cargo\n"); // Escrevendo o cabeÃ§alho do arquivo
+    Funcionario *aux = lista->inicio;
     while (aux != NULL)
     {
-        fprintf(arq, "%s;%s;%d;%d;%s;%s\n", aux->titulo, aux->autor, aux->mes, aux->ano, aux->idioma, aux->tipo); // Escrevendo os dados de cada livro no arquivo
+        fprintf(arq, "%s;%s;%d;%d;%f;%s\n", aux->cpf, aux->nome, aux->mes, aux->ano, aux->salario, aux->cargo); // Escrevendo os dados de cada funcionario no arquivo
         aux = aux->prox;
     }
     fclose(arq);
@@ -224,123 +224,124 @@ void cabecalho()
 // Criando uma função para mostrar o menu de opções do sistema CRUD
 void mostrar_menu()
 {
-    printf("Sistema CRUD de Livros\n");
+    printf("Sistema de administração de funcionarios\n");
     printf("Escolha uma opção:\n");
-    printf("1 - Criar um novo livro\n");
-    printf("2 - Ler um livro pelo título\n");
-    printf("3 - Atualizar um livro pelo título\n");
-    printf("4 - Deletar um livro pelo título\n");
-    printf("5 - Listar todos os livros\n");
+    printf("1 - Criar um novo cadastro\n");
+    printf("2 - Ler um cadastro pelo cpf\n");
+    printf("3 - Atualizar um cadastro\n");
+    printf("4 - Deletar um cadastro pelo cpf\n");
+    printf("5 - Listar todos os cadastros\n");
     printf("6 - Sair do sistema\n");
 }
 
 // Criando a função principal do programa
 int main()
 {
-    // Carregando os dados dos livros a partir do arquivo livros.txt
-    Lista *lista = carregar_arquivo("livros.txt");
-
-    // Variável para armazenar a opção escolhida pelo usuário
+    // Carregando os dados dos funcionarios a partir do arquivo funcionarios.txt
+    Lista *lista = carregar_arquivo("funcionarios.txt");
+    char cpf[11];
+    Funcionario *busca_cpfs = buscar_funcionario(lista, cpf);
+    // Variavel para armazenar a opção escolhida pelo usuario
     int opcao;
 
-    // Laço para repetir o menu até o usuário sair do sistema
+    // laço para repetir o menu até o usuario sair do sistema
     do
     {
         cabecalho();
-        mostrar_menu(); // Mostrando o menu de opções
+        mostrar_menu(); // Mostrando o menu de opção
         printf("Digite a sua opção: ");
         scanf("%d", &opcao);
         getchar();
         switch (opcao)
         {
-        case 1:                                  // Criar um novo livro
-            Livro *novo = ler_livro();           // Lendo os dados do livro a partir do teclado
-            inserir_final(lista, novo);          // Inserindo o livro no final da lista
-            salvar_arquivo("livros.txt", lista); // Salvando os dados da lista no arquivo
-            printf("Livro criado com sucesso.\n");
+        case 1:                                        // Criar um novo cadastro funcionario
+            Funcionario *novo = ler_funcionario();     // Lendo os dados do funcionario a partir do teclado
+            inserir_final(lista, novo);                // Inserindo o funcionario no final da lista
+            salvar_arquivo("funcionarios.txt", lista); // Salvando os dados da lista no arquivo
+            printf("Cadastro criado com sucesso.\n");
             sleep(1);
             system("cls");
             break;
-        case 2: // Ler um livro pelo título
-            char titulo[100];
-            printf("Digite o título do livro que deseja ler: ");
-            scanf("%[^\n]", titulo);
+        case 2: // Ler um cadastro pelo cpf
+            cpf[11];
+            printf("Digite o cpf do funcionario que deseja ler: ");
+            scanf("%[^\n]", cpf);
             getchar();
-            Livro *busca = buscar_titulo(lista, titulo); // Buscando o livro na lista pelo título
+            Funcionario *busca = busca_cpfs; // Buscando o funcionario na lista pelo cpf
             if (busca == NULL)
             {
-                printf("Livro não encontrado.\n");
+                printf("Funcionario não encontrado.\n");
             }
             else
             {
-                imprimir_livro(busca); // Imprimindo o livro na tela
+                imprimir_funcionario(busca); // Imprimindo o funcionario na tela
             }
             system("pause");
             system("cls");
             break;
-        case 3: // Atualizar um livro pelo título
-            titulo[100];
-            printf("Digite o título do livro que deseja atualizar: ");
-            scanf("%[^\n]", titulo);
+        case 3: // Atualizar um funcionario pelo cpf
+            cpf[11];
+            printf("Digite o cpf do cadastro que deseja atualizar: ");
+            scanf("%[^\n]", cpf);
             getchar();
-            Livro *atual = buscar_titulo(lista, titulo); // Buscando o livro na lista pelo título
+            Funcionario *atual = busca_cpfs; // Buscando o funcionario na lista pelo cpf
             if (atual == NULL)
             {
-                printf("Livro não encontrado.\n");
+                printf("Funcionario não encontrado.\n");
             }
             else
             {
-                Livro *novo = ler_livro();           // Lendo os novos dados do livro a partir do teclado
-                strcpy(atual->titulo, novo->titulo); // Copiando os novos dados para o livro antigo
-                strcpy(atual->autor, novo->autor);
+                Funcionario *novo = ler_funcionario(); // Lendo os novos dados do funcionario a partir do teclado
+                strcpy(atual->cpf, novo->cpf);         // Copiando os novos dados para o funcionario antigo
+                strcpy(atual->nome, novo->nome);
                 atual->mes = novo->mes;
                 atual->ano = novo->ano;
-                strcpy(atual->idioma, novo->idioma);
-                strcpy(atual->tipo, novo->tipo);
-                free(novo);                          // Liberando a memória do livro temporário
-                salvar_arquivo("livros.txt", lista); // Salvando os dados da lista no arquivo
-                printf("Livro atualizado com sucesso.\n");
+                atual->salario = novo->salario;
+                strcpy(atual->cargo, novo->cargo);
+                free(novo);                                // Liberando a memoria do funcionario temporario
+                salvar_arquivo("funcionarios.txt", lista); // Salvando os dados da lista no arquivo
+                printf("Cadastro atualizado com sucesso.\n");
             }
             sleep(1);
             system("cls");
             break;
-        case 4: // Deletar um livro pelo título
-            titulo[100];
-            printf("Digite o título do livro que deseja deletar: ");
-            scanf("%[^\n]", titulo);
+        case 4: // Deletar um funcionario pelo cpf
+            cpf[11];
+            printf("Digite o cpf do cadastro que deseja deletar: ");
+            scanf("%[^\n]", cpf);
             getchar();
-            int removido = remover_titulo(lista, titulo); // Removendo o livro da lista pelo título
+            int removido = remover_funcionario(lista, cpf); // Removendo o funcionario da lista pelo cpf
             if (removido == 0)
             {
-                printf("Livro não encontrado.\n");
+                printf("Cadastro não encontrado.\n");
             }
             else
             {
-                salvar_arquivo("livros.txt", lista); // Salvando os dados da lista no arquivo
-                printf("Livro deletado com sucesso.\n");
+                salvar_arquivo("funcionario.txt", lista); // Salvando os dados do funcionario no arquivo
+                printf("Cadastro deletado com sucesso.\n");
             }
             sleep(1);
             system("cls");
             break;
-        case 5:                    // Listar todos os livros
-            imprimir_lista(lista); // Imprimindo a lista de livros na tela
+        case 5:                    // Listar todos os funcionarios
+            imprimir_lista(lista); // Imprimindo a lista de funcionarios na tela
             system("pause");
             system("cls");
             break;
         case 6: // Sair do sistema
-            printf("Obrigado por usar o sistema CRUD de livros. Até mais!\n");
+            printf("Obrigado por usar o sistema de administração de funcionarios. Até mais!\n");
             sleep(1);
             system("cls");
             break;
-        default: // Opção inválida
-            printf("Opção inválida. Tente novamente.\n");
+        default: // opção invalida
+            printf("Opção inválida! Tente novamente.\n");
             sleep(1);
             system("cls");
         }
 
     } while (opcao != 6);
 
-    // Liberando a memória da lista de livros
+    // Liberando a memória da lista de funcionarios
     liberar_lista(lista);
 
     return 0;
