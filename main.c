ffdc515ab45b4b4df8d69fa57b1884304a56dd4d
leonardomb1 <lmbaudin2013@gmail.com>
2023-11-14 18:16:19 -0300

    Mudança de escopo do projeto, feito refatoração do código base para funcionar como mecanismo de consulta em banco de dados

--- main.c
+++ main.c
@@ -1,141 +1,170 @@
+#include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include "arquivar.h"
-#include "lista.h"
+#include <sql.h>
+#include <sqlext.h>
+#include <conio.h>
+#include "estruturas.h"
+#include "servico.h"
+#include "interface.h"
+#include "modulo.h"
 
-#define NOME_EMPRESA "Safe Start Ltd."
+void menuLogin();
 
-char *campos_funcionarios[] = {"CPF", "Nome", "Cargo", "Mes", "Ano", "Salario"};
-char *campos_produto[] = {"Codigo Produto", "Descricao Produto", "Armazem", "Endereco", "Custo Unitario"};
-
-void mostrar_menu_principal()
+void menuFuncionario()
 {
-    printf("Sistema de recursos empresariais - %s \n", NOME_EMPRESA);
-    printf("Escolha uma opcao:\n");
-    printf("1 - Menu de cadastro de funcionarios\n");
-    printf("2 - Menu de cadastro de produtos\n");    
-    printf("3 - Sair do Sistema\n");
-}
-
-void mostrar_menu_funcionario()
-{
-    printf("Sistema de administração de funcionarios\n");
-    printf("Escolha uma opção:\n");
-    printf("1 - Criar um novo cadastro\n");
-    printf("2 - Ler um cadastro pelo cpf\n");
-    printf("3 - Atualizar um cadastro\n");
-    printf("4 - Deletar um cadastro pelo cpf\n");
-    printf("5 - Listar todos os cadastros\n");
-    printf("6 - Sair do modulo\n");
-}
-
-void menu_principal();
-
-void menu_funcionario()
-{
-    char arquivo_funcionarios[20] = "funcionarios.txt";
-    Lista *lista = carregar_arquivo(arquivo_funcionarios, sizeof(Funcionario), (void (*)(Lista *, void *))add);
-    char cpf[11];
     int opcao;
 
     do
     {
-        mostrar_menu_funcionario();
-        printf("Digite a sua opção: ");
+        cabecaMenuFuncionario();
+        printf("Digite a sua opcao: ");
         scanf(" %i", &opcao);
         getchar();
 
         switch (opcao)
         {
-            case 1:
-                criar();
-                novo_registro(lista, sizeof(Funcionario), campos_funcionarios, arquivo_funcionarios, ler_dados, add);
-                sleep(1);
-                system("cls");
-                break;
-
-            case 2:
-                printf("Digite o cpf do funcionario que deseja ler: ");
-                scanf("%[^\n]", cpf);
-                getchar();
-                Funcionario *busca = buscar(lista, sizeof(Funcionario), campos_funcionarios, cpf, "CPF");
-                if (busca == NULL)
-                {
-                    printf("Funcionario não encontrado.\n");
-                }
-                else
-                {
-                    imprimir_struct(busca, sizeof(Funcionario), campos_funcionarios);
-                }
-                system("cls");
-                break;
-
-            case 3:
-                atualizar_dados(lista, sizeof(Funcionario), campos_funcionarios, "cpf", buscar, ler_dados);
-                getchar();
-                sleep(1);
-                system("cls");
-                break;
-
-            case 4:
-                remover_dados(lista, sizeof(Funcionario), campos_funcionarios, "cpf", buscar);
-                sleep(1);
-                system("cls");
-                break;
-
-            case 5:
-                imprimir_lista(lista, sizeof(Funcionario), campos_funcionarios);
-                printf("Pressione qualquer botao para sair.");
-                getchar();
-                system("cls");
-                break;
-
-            case 6:
-                menu_principal();
-                system("cls");
-                break;
-
-            default:
-                printf("Opção inválida! Tente novamente.\n");
-                sleep(1);
-                system("cls");
-
+        case 1:
+            moduloFuncionario(1);
+            break;
+        case 2:
+            moduloFuncionario(2);
+            break;
+        case 3:
+            moduloFuncionario(3);
+            break;
+        case 4:
+            moduloFuncionario(4);
+            break;
+        case 5:
+            moduloFuncionario(5);
+            break;
+        case 6:
+            system("cls");
+            break;
+        default:
+            printf(msg_erro_entrada);
+            sleep(1);
+            system("cls");
+            break;
         }
-
     } while (opcao != 6);
-
-    liberar(lista, sizeof(Funcionario));
 }
 
-void menu_principal()
+void menuPrincipal()
 {
     int opcao;
     do
     {
-        mostrar_menu_principal();
+        cabecaMenuInicio();
         printf("Digite a sua opcao: ");
         scanf(" %i", &opcao);
         getchar();
         switch (opcao)
         {
         case 1:
-            menu_funcionario();
+            system("cls");
+            menuFuncionario();
             break;
         case 2:
             //menu_produto();
+            system("cls");
+            break;
+        case 3:
+            exit(1);
+            break;
+        case 4:
+            system("cls");
+            menuLogin();
             break;
         default:
-            printf("Opcao invalida! Tente novamente.\n");
+            printf(msg_erro_entrada);
             sleep(1);
             system("cls");
+            break;
         }
-    } while (opcao != 3);
+    } while (opcao != 3 || opcao !=4);
+}
+
+void centralizaTextoPdAmbos(char *texto, int largura) {
+    int pad = (largura - strlen(texto)) / 2;
+    printf("%*s%s%*s", pad, "", texto, pad, "");
+}
+
+void centralizaTextoPdEsquerda(char *texto, int largura) {
+    int pad = (largura - strlen(texto)) / 2;
+    printf("%*s%s", pad, "", texto);
+}
+
+void menuLogin() {
+    iniciarServico();
+    system("cls");
+    char barra[] = "#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#";
+    int largura = 125;
+    do
+    {
+        int i = 0;
+        char ch;
+        usrid = 0;
+        for(int i = 0; i<4; i++) {centralizaTextoPdAmbos(barra, largura); printf("\n");}
+
+        centralizaTextoPdEsquerda("Usuario: ", largura);
+        scanf("%s", usuario);
+        
+        centralizaTextoPdEsquerda("Senha: ", largura);
+
+        while (1) {
+            ch = _getch();
+            if (ch == '\r') { 
+                break;
+            } else if (isprint(ch)) {
+                senha[i] = ch;
+                i++;
+                printf("*");
+            } else if (ch == '\b') {
+                if (i > 0) {
+                    printf("\b \b");
+                    i--;
+                }
+            }
+        }
+
+    senha[i] = '\0';
+
+        printf("\n");
+        centralizaTextoPdEsquerda("", 100);
+        validacaoLogin(usuario, senha);
+        if (usrid==0)
+        {
+            centralizaTextoPdEsquerda("", 100);
+            printf("  Usuario nao Autorizado!\n");
+            system("pause");
+            system("cls");
+        }
+
+    } while (usrid == 0);
+
+    liberarServico();
+    
+    for (int frame = 0; frame < sizeof(nome_empresa_banner) / sizeof(nome_empresa_banner[0]); frame++) {
+        centralizaTextoPdAmbos("",30);
+        printf(ANSI_COLOR_YELLOW "%s\n" ANSI_COLOR_RESET, nome_empresa_banner[frame]);
+        fflush(stdout);
+        usleep(300000);
+    }
+
+    printf("\n");
+
+    printf("Pressione qualquer tecla para continuar...");
+    system("pause");
+    system("cls");
+    menuPrincipal();
 }
 
 int main()
 {
-    menu_principal();
+    menuLogin();
     return 0;
 }
